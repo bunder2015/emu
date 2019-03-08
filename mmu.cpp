c1ddce8861a0cc4a984daf16d1062ec806428812
bunder2015 <omfgbunder@gmail.com>
2019-03-08 18:08:36 -0500

    go back to working

--- mmu.cpp
+++ mmu.cpp
@@ -1,9 +1,48 @@
-#include "rom.h"        // for rom_ingest rom_headerparse
+#include <iostream>     // for std::cout
+#include <fstream>      // for std::ifstream
+#include <cstring>      // for memcmp
+
+using std::cout;
+using std::ifstream;
 
 char* rombuffer;        // in memory copy of the entire ROM file
 
+int rom_ingest(char* romfile) {
+    ifstream rf;                // handler for ROM file opening
+    unsigned long filesize;     // size of ROM file
+
+    rf.open(romfile, ifstream::binary);     // open file
+
+    if (rf.good()) {
+        rf.seekg(0, ifstream::end);         // go to end of file
+        filesize = rf.tellg();              // read size of ROM file
+        rf.seekg(0, ifstream::beg);         // go back to beginning of file
+        rombuffer = new char[filesize];     // rombuffer holds the file while we read the header and prepare the memory map
+        rf.read(rombuffer, filesize);       // read file to rombuffer
+        rf.close();                         // close file
+        return 0; /// segfault here (unknown)
+    } else {
+        cout << "ERROR: File could not be read!\n";
+        return 1;
+    }
+}
+
+int rom_headerparse() {
+// TODO (chris#9#): consider adding pre iNES ROM support
+    static const char headermagic[4] = { 0x4E, 0x45, 0x53, 0x1A };  // "N", "E", "S", 0x1A
+
+    /// or segfault here (rombuffer null)
+    if (memcmp(rombuffer, headermagic, sizeof(headermagic)) == 0) { // compare header magic bytes
+// TODO (chris#1#): verify iNES v1 vs v2, break down header elements to choose memory mapper and prepare CPU/PPU memory map
+        return 0;
+    } else {
+        cout << "ERROR: Not an iNES file! (missing magic)\n";
+        return 1;
+    }
+}
+
 int mmu_init(char* romfile) {
-    if (rom_ingest(romfile, rombuffer) == 0 && rom_headerparse(rombuffer) == 0) {
+    if (rom_ingest(romfile) == 0 && rom_headerparse() == 0) {
 // TODO (chris#2#): something after headerparse
         return 0;
     } else {
--- rom.h
+++
@@ -1,7 +0,0 @@
-#ifndef ROM_H_INCLUDED
-#define ROM_H_INCLUDED
-
-int rom_ingest(char* romfile, char* rombuffer);
-int rom_headerparse(char* rombuffer);
-
-#endif // ROM_H_INCLUDED
