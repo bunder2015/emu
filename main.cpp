f6f16a21d820959ff79e57089685e1b3085f730e
bunder2015 <omfgbunder@gmail.com>
2019-03-08 13:11:44 -0500

    initial push

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>     // for std::cout
+
+#include "mmu.h"        // for mmu_init
+
+using std::cout;
+
+int main(int argc, char* argv[]) {
+    char* romfile;          // ROM filename for input
+
+    if (argc > 2) {
+        cout << "ERROR: Too many arguments.\n";
+        return 1;
+    } else {
+        romfile = argv[1];
+    }
+
+    if (romfile != NULL) {
+        if (mmu_init(romfile) == 0) { // load ROM file and initialize memory map
+// TODO (chris#6#): CPU/APU/PPU
+        } else {
+            return 1;
+        }
+    } else {
+        cout << "ERROR: ROM filename not given.\n";
+        return 1;
+    }
+
+    // end
+    return 0;
+}
---
+++ mmu.h
@@ -0,0 +1,6 @@
+#ifndef MMU_H_INCLUDED
+#define MMU_H_INCLUDED
+
+int mmu_init(char* romfile);
+
+#endif // MMU_H_INCLUDED
