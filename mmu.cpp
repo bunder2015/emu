b81bcc3c85f377b3ddc25dd69718868d431eb88d
bunder2015 <omfgbunder@gmail.com>
2019-03-10 15:47:13 -0400

    headerparse prototype

--- mmu.cpp
+++ mmu.cpp
@@ -1,7 +1,8 @@
 #include "rom.h"    // for rom_ingest rom_headerparse
 
 int mmu_init(char* romfile) {
-    char* rombuffer;     // in memory copy of the entire ROM file
+    char* rombuffer;    // in memory copy of the entire ROM file
+    int mapper;         // iNES mapper number
 
     if (rom_ingest(romfile, &rombuffer) == 0 && rom_headerparse(&rombuffer) == 0) {
 // TODO (chris#2#): something after headerparse
