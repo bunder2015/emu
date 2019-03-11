452639e556dc8c75e4f20ad61eb7085888062d49
bunder2015 <omfgbunder@gmail.com>
2019-03-11 11:13:20 -0400

    fix pointers in headerparse

--- rom.cpp
+++ rom.cpp
@@ -31,20 +31,19 @@ int rom_headerparse(char** rombuffer) {
     int inesformat = 0;
 // TODO (chris#9#): consider adding "archiac" iNES ROM support
     if (memcmp(*rombuffer, headermagic, sizeof(headermagic)) == 0) {    // compare header magic bytes
-// TODO (chris#1#): verify iNES v1 vs v2, break down header elements to choose memory mapper and prepare CPU/PPU memory map
-// TODO (chris#1#): this doesn't work, yay pointers again
-//        if (((**rombuffer + 7) & 0x0C) == 0x08) {   // if byte 7, bits 8 and 4, are (1,0)
-//            inesformat = 2;                     // we are iNES format 2.0
-//        } else if ((((*rombuffer + 7) & 0x0C) == 0)    // if byte 7, bits 8 and 4, are (0,0)
-//                    && (((*rombuffer + 0x0C) & 0x00) == 0)   // and bytes 12-15 are 0
-//                    && (((*rombuffer + 0x0D) & 0x00) == 0)
-//                    && (((*rombuffer + 0x0E) & 0x00) == 0)
-//                    && (((*rombuffer + 0x0F) & 0x00) == 0)) {
-//            inesformat = 1;                     // we are iNES format 0.7 or 1.0
-//        } else {
-//            cout << "ERROR: Not an iNES 0.7/1.0 or 2.0 file!\n";
-//            return 1;
-//        }
+        if ((*(*rombuffer + 0x07) & 0x0C) == 0x08) {                // if byte 7, bits 8 and 4, are (1,0)
+            inesformat = 2;                                         // we are iNES format 2.0
+        } else if (    ((*(*rombuffer + 0x07) & 0x0C) == 0x00)      // if byte 7, bits 8 and 4, are (0,0)
+                    && ((*(*rombuffer + 0x0C) & 0xFF) == 0x00)      // and bytes 12-15 are 0
+                    && ((*(*rombuffer + 0x0D) & 0xFF) == 0x00)
+                    && ((*(*rombuffer + 0x0E) & 0xFF) == 0x00)
+                    && ((*(*rombuffer + 0x0F) & 0xFF) == 0x00)) {
+            inesformat = 1;                                         // we are iNES format 0.7 or 1.0
+        } else {
+            cout << "ERROR: Not an iNES 0.7/1.0 or 2.0 file! (good magic, header trashed)\n";
+            return 1;
+        }
+// TODO (chris#1#): break down header elements to choose memory mapper and prepare CPU/PPU memory map
         return 0;
     } else {
         cout << "ERROR: Not an iNES file! (missing magic)\n";
